e903def87015a047063986535fbb7af5b2ccf61d
mgur <mgur@pop-os.localdomain>
2023-10-05 09:18:52 +0200

    welcome apge

--- src/routes/tenant.php
+++ src/routes/tenant.php
@@ -26,4 +26,8 @@ Route::middleware([
     Route::get('/', function () {
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
+
+    Route::get('/dashboard', function () {
+        return view('welcome');
+    });
 });
